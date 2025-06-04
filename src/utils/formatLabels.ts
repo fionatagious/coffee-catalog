959ca7d96d3245817528051a38ad8c2956bfbd17
Fiona Tang <ftang@codeforamerica.org>
2025-06-04 13:24:40 -0500

    unit test src/utils/formatLabels.ts

--- src/utils/formatLabels.ts
+++ src/utils/formatLabels.ts
@@ -1,5 +1,4 @@
 const formatLabels = (keysArray: string[]) => {
-  console.log("Original labels:", keysArray);
   // Convert snake_case to Capital Case
   // e.g. "coffee_leaf_rust" -> "Coffee leaf rust"
   const resultArray = keysArray.map((element) => {
@@ -7,7 +6,6 @@ const formatLabels = (keysArray: string[]) => {
       element.charAt(0).toUpperCase() + element.slice(1).replaceAll("_", " ")
     );
   });
-  console.log("Formatted labels:", resultArray);
   return resultArray;
 };
 
